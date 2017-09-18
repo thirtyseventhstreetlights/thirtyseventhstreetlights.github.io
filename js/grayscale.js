c5c5fdddff46ee6129fbaab34731f460d70a5bb0
37th St Lights <thirtyseventhstreetlights@gmail.com>
2017-09-17 21:20:12 -0500

    Updated map to focus on 37th St
    30.301905, -97.738321

--- js/grayscale.js
+++ js/grayscale.js
@@ -42,7 +42,7 @@ var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
-  map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
+  map.setCenter(new google.maps.LatLng(30.301905, -97.738321));
 });
 
 function init() {
